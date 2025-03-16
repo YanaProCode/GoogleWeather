93f3dca6748f626fbe1cd991d8464719dc55a27d
Yana Proshak <yana_proshak@epam.com>
2025-03-16 21:04:06 +0100

    fixed issue with cokiies and captcha

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,28 +1,25 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
 import time
 
 def test_weather_above_zero():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=500)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
+        stealth_sync(page)
         page.goto("https://www.google.com/")
-        #time.sleep(2)
+        time.sleep(2)
+
         try:
-            page.wait_for_selector("text=Zaakceptuj wszystko", timeout=10000)
-            page.click("text=Zaakceptuj wszystko")
-            print("Clicked on the cookie consent button.")
-            page.screenshot(path="after_click.png")
-        except Exception as e:
-            print("Cookie consent button not found or not clickable:", e)
-            page.screenshot(path="error.png")
-        #time.sleep(5)
-        try:
-            search_input_xpath = "/html/body/div[1]/div[3]"
+            search_input_xpath = "//*[@id=\"APjFqb\"]"
             page.wait_for_selector(f"xpath={search_input_xpath}", timeout=10000)
-            page.fill(f"xpath={search_input_xpath}", "weather Wroclaw")
-            page.press(f"xpath={search_input_xpath}", "Enter")
+            page.fill(search_input_xpath, "weather Wroclaw")
+            time.sleep(2)
+            page.press(search_input_xpath, "Enter")
+            time.sleep(2)
             page.wait_for_selector("#wob_tm")
+            time.sleep(2)
 
             temperature = page.inner_text("#wob_tm")
             assert int(temperature) > 0, f"Expected temperature to be above 0, but got {temperature}"
